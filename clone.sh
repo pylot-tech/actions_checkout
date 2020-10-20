e15a5d9034a2f72f8e077f09c1d3c5ffa12f4f9c
Sebastian Freitag <sebastian.freitag@pylot.tech>
2020-10-20 18:51:45 +0200

    add robustness

--- clone.sh
+++ clone.sh
@@ -14,7 +14,18 @@ fi
 
 # Clone the main repo (that is subject of the build)
 IFS='/' read -ra ARRC <<< "${REPOSITORY}"
-git clone --depth 1 -b ${BRANCH} https://${HTTPS_PAT}@github.com/${REPOSITORY} ./src/${ARRC[1]}
+
+# github sometimes times out on the initial clone attempt.
+# let's just try again a few times in this case.
+n=0
+until [ "$n" -ge 5 ]
+do
+   git clone --depth 1 -b ${BRANCH} https://${HTTPS_PAT}@github.com/${REPOSITORY} ./src/${ARRC[1]} && break
+   n=$((n+1)) 
+   sleep 5
+done
+
+
 
 IFS=';' read -ra ARRA <<< "${TALOS_DEPS}"
 
@@ -22,9 +33,22 @@ for i in "${ARRA[@]}"; do
     IFS=':' read -ra ARRB <<< "$i"
     if [ -z "${ARRB[1]}" ]
     then
-        git clone --depth 1 https://${HTTPS_PAT}@github.com/pylot-tech/${ARRB[0]}.git ./src/${ARRB[0]}    
+        n=0
+        until [ "$n" -ge 5 ]
+        do
+            git clone --depth 1 https://${HTTPS_PAT}@github.com/pylot-tech/${ARRB[0]}.git ./src/${ARRB[0]} && break
+            n=$((n+1)) 
+            sleep 5
+        done
+        
     else
-        git clone --depth 1 -b ${ARRB[1]} https://${HTTPS_PAT}@github.com/pylot-tech/${ARRB[0]}.git ./src/${ARRB[0]} 
+        n=0
+        until [ "$n" -ge 5 ]
+        do
+            git clone --depth 1 -b ${ARRB[1]} https://${HTTPS_PAT}@github.com/pylot-tech/${ARRB[0]}.git ./src/${ARRB[0]} && break
+            n=$((n+1)) 
+            sleep 5
+        done 
     fi    
 done
 
